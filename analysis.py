976542d0e39682a116087427054b60f4e4f1571c
Raman <namansingh982150@gmail.com>
2025-12-13 20:55:50 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"C:\Users\hp\OneDrive\Desktop\pollution data\city_day.csv")
+df = pd.read_csv(r"data/city_day.csv")
 
 #-----------------------
 # Filling missing values
@@ -89,4 +89,5 @@ plt.xlabel("YERA")
 plt.ylabel("CITY")
 plt.tight_layout()
 plt.show()
+
 
